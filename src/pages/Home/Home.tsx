bf6abc9872f8965ea6386dc19bc1f9cf4e6cf631
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-02-13 14:51:59 -0300

    style: background image, glass effect

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Grid, Text, TextInput, ActionIcon } from '@mantine/core'
+import { Grid, Text, TextInput, ActionIcon, Center, BackgroundImage } from '@mantine/core'
 import { IconSearch } from '@tabler/icons'
 
 import Logo from '@/assets/logo.svg'
@@ -9,23 +9,32 @@ function HomePage() {
     const { classes } = useStyles()
 
     return (
-        <Grid>
-            <Grid.Col span={12}>
-                <img src={Logo} alt="logo picture" className={classes.logo} />
-                <Text component="h1">
-                    Guia<strong>Livreiro</strong>
-                </Text>
+        <BackgroundImage
+            style={{ height: '100vh' }}
+            src={'https://static-cse.canva.com/blob/921439/ImagebyStanislavKondratievviaUnsplash.35c0d8f7.avif'}>
+            <Grid style={{ margin: '0' }}>
+                <Grid.Col span={12}>
+                    <Center className={classes.container}>
+                        <div className={classes.glass} />
+                        <img src={Logo} alt="logo picture" className={classes.logo} />
+                        <Text ta="center" className={classes.title}>
+                            Guia<strong>Livreiro</strong>
+                        </Text>
 
-                <TextInput
-                    placeholder="Procure o nome do livro"
-                    rightSection={
-                        <ActionIcon>
-                            <IconSearch size={20} />
-                        </ActionIcon>
-                    }
-                />
-            </Grid.Col>
-        </Grid>
+                        <TextInput
+                            className={classes.input}
+                            placeholder="Procure o nome do livro"
+                            radius="lg"
+                            rightSection={
+                                <ActionIcon>
+                                    <IconSearch size={20} />
+                                </ActionIcon>
+                            }
+                        />
+                    </Center>
+                </Grid.Col>
+            </Grid>
+        </BackgroundImage>
     )
 }
 
